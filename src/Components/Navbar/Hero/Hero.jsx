26c9ccfd448da0167f317ec6c2beb1cc1647a0bc
John Doe <a.flavio4366@gmail.com>
2024-08-14 23:29:20 +0200

    website_update

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-	<div className='hero'>
-	  
+	<div className='hero container'>
+	  <div className="hero-text">
+		<h1>We Ensure better education for a better world</h1>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+		<button className='btn'>EXPLORE MORE <img src={dark_arrow} alt=""/></button>
+	  </div>
 	</div>
   )
 }
