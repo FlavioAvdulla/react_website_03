0f16e192bb8694a1878a79bdd9213de850d6fa9a
John Doe <a.flavio4366@gmail.com>
2024-08-16 10:41:40 +0200

    website update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,18 +3,17 @@ import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-
-  const [sticky, setSticky] = userState(false);
+	
+  const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
   }, []);
 
   return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-		
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>HOME</li>
