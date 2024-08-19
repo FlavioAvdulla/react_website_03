1d57cdb2612f59ad251514dc3ed405f4904c928b
John Doe <a.flavio4366@gmail.com>
2024-08-19 13:38:50 +0200

    website update

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -10,38 +10,33 @@ import user_4 from "../../assets/user-4.png";
 const Testimonials = () => {
   const slider = useRef();
   let tx = 0;
+
   const slideForward = () => {
-  if(tx > -50) {
-	tx -= 25;
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
   }
-  slider.current.style.transform = `translateX(${tx}%)`
-};
-if(tx < 0) {
-	tx += 25;
-  }
-  slider.current.style.transform = `translateX(${tx}%)`
-};
 
   const slideBackward = () => {
-};
+    if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  }
 
   return (
     <div className="testimonials">
       <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
-      <img
-        src={back_icon}
-        alt=""
-        className="back-btn"
-        onClick={slideBackward}
-      />
-      <div className="slider">
+      <img src={back_icon} alt="" className="back-btn" onClick={slideBackward} />
+	  <div className="slider">
         <ul ref={slider}>
           <li>
             <div className="slide">
               <div className="user-info">
                 <img src={user_1} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 1</h3>
                   <span>Edusity, USA</span>
                 </div>
               </div>
@@ -58,7 +53,7 @@ if(tx < 0) {
               <div className="user-info">
                 <img src={user_2} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 2</h3>
                   <span>Edusity, USA</span>
                 </div>
               </div>
@@ -75,7 +70,7 @@ if(tx < 0) {
               <div className="user-info">
                 <img src={user_3} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 3</h3>
                   <span>Edusity, USA</span>
                 </div>
               </div>
@@ -92,7 +87,7 @@ if(tx < 0) {
               <div className="user-info">
                 <img src={user_4} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 4</h3>
                   <span>Edusity, USA</span>
                 </div>
               </div>
