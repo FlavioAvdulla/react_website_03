dcb3ac1994a931c3f86dc33390ec1c41dea6858a
John Doe <a.flavio4366@gmail.com>
2024-08-19 09:26:43 +0200

    website_update

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,18 +1,92 @@
 import React from "react";
 import "./Testimonials.css";
-import next_icon from "../../assets/next_icon.png";
-import back_icon from "../../assets/back_icon.png";
+import next_icon from "../../assets/next-icon.png";
+import back_icon from "../../assets/back-icon.png";
 import user_1 from "../../assets/user-1.png";
 import user_2 from "../../assets/user-2.png";
 import user_3 from "../../assets/user-3.png";
 import user_4 from "../../assets/user-4.png";
 
 const Testimonials = () => {
+  const slideForward = () => {};
+  const slideBackward = () => {};
+
   return (
     <div className="testimonials">
-      <img src={next_icon} alt="" className="next-btn" />
-      <img src={back_icon} alt="" className="back-btn" />
-	  <div className="slider"></div>
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon} alt="" className="back-btn" onClick={slideBackward} />
+      <div className="slider">
+        <ul>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer.
+              </p>
+            </div>
+          </li>
+          {/*===================================================*/}
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer.
+              </p>
+            </div>
+          </li>
+          {/*===================================================*/}
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer.
+              </p>
+            </div>
+          </li>
+          {/*===================================================*/}
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
