e5c5bf72eacbe7e727c5884b621955d65083512d
John Doe <a.flavio4366@gmail.com>
2024-08-20 11:44:34 +0200

    website update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,8 +4,34 @@ import msg_icon from "../../assets/msg-icon.png";
 import mail_icon from "../../assets/mail-icon.png";
 import phone_icon from "../../assets/phone-icon.png";
 import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending...");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "6051b6b5-751e-4800-8de9-89c9c0bb27e9");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className="contact">
       <div className="contact-col">
@@ -18,12 +44,49 @@ const Contact = () => {
           since the 1500s, when an unknown printer.
         </p>
         <ul>
-          <li><img src={mail_icon} alt="" />contact@greatstack.dev</li>
-          <li><img src={phone_icon} alt="" />+355 67 63 11 918</li>
-          <li><img src={location_icon} alt="" />Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522</li>
+          <li>
+            <img src={mail_icon} alt="" />
+            contact@greatstack.dev
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            +355 67 63 11 918
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522
+          </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            SUBMIT NOW <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
