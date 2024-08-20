ca85191294f7daa6157c2bf28e2ac03542f72abd
John Doe <a.flavio4366@gmail.com>
2024-08-20 14:58:19 +0200

    website update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -34,29 +34,31 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <div className="contact-col">
-        <h3>
-          Send us a message <img src={msg_icon} alt="" />
-        </h3>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer.
-        </p>
-        <ul>
-          <li>
-            <img src={mail_icon} alt="" />
-            contact@greatstack.dev
-          </li>
-          <li>
-            <img src={phone_icon} alt="" />
-            +355 67 63 11 918
-          </li>
-          <li>
-            <img src={location_icon} alt="" />
-            Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522
-          </li>
-        </ul>
+      <div className="message-col">
+        <div className="contact-col">
+          <h3>
+            Send us a message <img src={msg_icon} alt="" />
+          </h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer.
+          </p>
+          <ul>
+            <li>
+              <img src={mail_icon} alt="" />
+              contact@greatstack.dev
+            </li>
+            <li>
+              <img src={phone_icon} alt="" />
+              +355 67 63 11 918
+            </li>
+            <li>
+              <img src={location_icon} alt="" />
+              Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="contact-col">
         <form onSubmit={onSubmit}>
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright © 2024 Flavio Avdulla All rights reserved.</p>
+      <ul>
+        <li>Terms of Service</li>
+        <li>Terms of Service</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
