a1fb37f577eddf6ff55a4bd9c655835bcdc85ca8
John Doe <a.flavio4366@gmail.com>
2024-08-20 19:38:59 +0200

    website update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
@@ -6,21 +7,49 @@ const Navbar = () => {
   const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY > 660 ? setSticky(true) : setSticky(false);
-    });
+    const handleScroll = () => {
+      window.scrollY > 600 ? setSticky(true) : setSticky(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
-        <li><link to="hero" smooth={true} offset={0} duration={500}>HOME</link></li>
-        <li><link to="programs" smooth={true} offset={0} duration={500}>PROGRAM</link></li>
-        <li><link to="about" smooth={true} offset={0} duration={500}>ABOUT US</link></li>
-        <li><link to="campus" smooth={true} offset={0} duration={500}>CAMPUS</link></li>
-        <li><link to="testimonials" smooth={true} offset={0} duration={500}>TESTIMONIALS</link></li>
-        <li><button className="btn_02"><link to="contact" smooth={true} offset={0} duration={500}>CONTACT US</link></button></li>
+        <li>
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            HOME
+          </Link>
+        </li>
+        <li>
+          <Link to="program" smooth={true} offset={0} duration={500}>
+            PROGRAM
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={0} duration={500}>
+            ABOUT US
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={0} duration={500}>
+            CAMPUS
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonials" smooth={true} offset={0} duration={500}>
+            TESTIMONIALS
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} offset={0} duration={500} className="btn_02">
+            CONTACT US
+          </Link>
+        </li>
       </ul>
     </nav>
   );
