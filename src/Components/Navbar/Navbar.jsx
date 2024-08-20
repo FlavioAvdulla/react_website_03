bd6dec03537e0b462a81a7621a504fc4e6d3ad25
John Doe <a.flavio4366@gmail.com>
2024-08-20 17:56:58 +0200

    website update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,14 +15,12 @@ const Navbar = () => {
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
-        <li>HOME</li>
-        <li>PROGRAM</li>
-        <li>ABOUT US</li>
-        <li>CAMPUS</li>
-        <li>TESTIMONIALS</li>
-        <li>
-          <button className="btn">CONTACT US</button>
-        </li>
+        <li><link to="hero" smooth={true} offset={0} duration={500}>HOME</link></li>
+        <li><link to="programs" smooth={true} offset={0} duration={500}>PROGRAM</link></li>
+        <li><link to="about" smooth={true} offset={0} duration={500}>ABOUT US</link></li>
+        <li><link to="campus" smooth={true} offset={0} duration={500}>CAMPUS</link></li>
+        <li><link to="testimonials" smooth={true} offset={0} duration={500}>TESTIMONIALS</link></li>
+        <li><button className="btn_02"><link to="contact" smooth={true} offset={0} duration={500}>CONTACT US</link></button></li>
       </ul>
     </nav>
   );
